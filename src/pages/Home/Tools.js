f73486f2ae79ef6eb88d887dc0540ad87b7b4a4b
sumonbarai <sumonbarai78@gmail.com>
2022-05-23 23:32:33 +0600

    product fetch in server

--- src/pages/Home/Tools.js
+++ src/pages/Home/Tools.js
@@ -1,40 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Product from "./Product";
 
 const Tools = () => {
-  const products = [
-    {
-      picture: "https://i.ibb.co/fMzZ6L8/fan-regulator-dimmer.jpg",
-      name: "Fan regulator",
-      available: "50",
-      price: "75",
-      description:
-        "Electric Switches. Fixit has the largest collection of all types of best quality switches in BD including Four Gang Switch, Switch for Motor, Floating Switch etc.",
-    },
-    {
-      picture: "https://i.ibb.co/fMzZ6L8/fan-regulator-dimmer.jpg",
-      name: "Fan regulator",
-      available: "50",
-      price: "75",
-      description:
-        "Electric Switches. Fixit has the largest collection of all types of best quality switches in BD including Four Gang Switch, Switch for Motor, Floating Switch etc.",
-    },
-    {
-      picture: "https://i.ibb.co/fMzZ6L8/fan-regulator-dimmer.jpg",
-      name: "Fan regulator",
-      available: "50",
-      price: "75",
-      description:
-        "Electric Switches. Fixit has the largest collection of all types of best quality switches in BD including Four Gang Switch, Switch for Motor, Floating Switch etc.",
-    },
-  ];
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const url = "http://localhost:5000/product";
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+  console.log(products);
   return (
     <section className="section-padding">
       <div className="tools-area container mx-auto px-4">
         <h2 className="section-title">our tools </h2>
         <div className="all-product grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {products.map((product, index) => (
-            <Product key={index} product={product}></Product>
+          {products.map((product) => (
+            <Product key={product._id} product={product}></Product>
           ))}
         </div>
       </div>

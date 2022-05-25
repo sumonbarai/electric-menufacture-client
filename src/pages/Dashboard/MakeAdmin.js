6ec8c047127fccc8369f60711c1d24aceb2d58d4
sumonbarai <sumonbarai78@gmail.com>
2022-05-26 01:41:42 +0600

    make admin page completed

--- src/pages/Dashboard/MakeAdmin.js
+++ src/pages/Dashboard/MakeAdmin.js
@@ -1,9 +1,104 @@
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import toast from "react-hot-toast";
+import { useQuery } from "react-query";
+import auth from "../../firebase.init";
+import Spinner from "../../shared/Spinner/Spinner";
 
 const MakeAdmin = () => {
+  const {
+    data: allUser,
+    isLoading,
+    refetch,
+  } = useQuery("order", () => {
+    return fetch(`http://localhost:5000/user`).then((res) => res.json());
+  });
+
+  if (isLoading) {
+    return <Spinner></Spinner>;
+  }
+  const handleMakeAdmin = (email) => {
+    const confirm = window.confirm("Are You sure make admin ?");
+    if (confirm) {
+      const roll = { roll: "admin" };
+      fetch(`http://localhost:5000/user/${email}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(roll),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.acknowledged) {
+            toast.success(`${email} is now Admin`);
+            refetch();
+          }
+        });
+    }
+  };
+  const handleRemoveAdmin = (email) => {
+    const confirm = window.confirm("Are You sure remove admin ?");
+    if (confirm) {
+      const roll = { roll: "" };
+      fetch(`http://localhost:5000/user/${email}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(roll),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.acknowledged) {
+            toast.success(`${email} admin access remove`);
+            refetch();
+          }
+        });
+    }
+  };
+
   return (
     <div>
-      <h1>this is make admin page</h1>
+      <h2 className="text-3xl text-center uppercase mb-8">manage admin user</h2>
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th>Serial</th>
+              <th>User Email</th>
+              <th>Make Admin</th>
+              <th>Remove Admin</th>
+            </tr>
+          </thead>
+          <tbody>
+            {allUser.map((singleUser, index) => (
+              <tr key={index}>
+                <th>{index + 1}</th>
+                <td>{singleUser.email}</td>
+                <td>
+                  <button
+                    onClick={() => handleMakeAdmin(singleUser.email)}
+                    className="btn btn-xs btn-secondary"
+                    disabled={singleUser.roll ? true : false}
+                  >
+                    Make Admin
+                  </button>
+                </td>
+                <td>
+                  <button
+                    onClick={() => handleRemoveAdmin(singleUser.email)}
+                    disabled={singleUser.roll ? false : true}
+                    className="btn btn-xs btn-secondary"
+                  >
+                    Remove Admin
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
